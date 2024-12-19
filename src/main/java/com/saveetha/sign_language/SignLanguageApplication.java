01660b17f83dbbfacd9d8e0505c10ae5bca6904c
veeramani-dev <veeramaniamk@gmail.com>
2024-12-19 20:00:55 +0530

    first commit

---
+++ src/main/java/com/saveetha/sign_language/SignLanguageApplication.java
@@ -0,0 +1,13 @@
+package com.saveetha.sign_language;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignLanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignLanguageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saveetha/sign_language/SignLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saveetha.sign_language;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignLanguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
